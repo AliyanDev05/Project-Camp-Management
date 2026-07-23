8346a74ea0fb57fa4ca7f1cdc588b19a11f36bd5
AliyanDev05 <aliyanabbas2812@gmail.com>
2026-07-23 22:56:05 +0500

    [PCM-0002] added the nodemon and dotenv structure and setup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("First Backend Project");
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env"
+});
+
+let myusername = process.env.myname
+
+console.log("value:", myusername);
