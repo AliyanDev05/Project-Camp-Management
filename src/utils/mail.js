70fa71fd29ce9610ef9f2a43e0d1b4006c2e68d5
AliyanDev05 <aliyanabbas2812@gmail.com>
2026-08-06 12:24:25 +0500

    [PCM-0011] implemented the email sending logic using nodemail

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,5 +1,45 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
 
+const sendMail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Task manager",
+      link: "https://taskmanagelink.com",
+    },
+  });
+
+  const emailtextual = mailGenerator.generatePlaintext(options.mailgenContent);
+
+  const emailHtml = mailGenerator.generate(options.mailgenContent);
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_HOST,
+    port: process.env.MAILTRAP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASS,
+    },
+  });
+
+  const mail = {
+    from: "mailtaskmanager@example.com",
+    to: options.email,
+    subject: options.subject,
+    text: emailtextual,
+    html: emailHtml,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+  } catch (error) {
+    console.error(
+      "email is not sent kindly check you creds of MAILTRAP and env file also",
+    );
+    console.error("Error:", error);
+  }
+};
 const emailVerificationMailgenContent = (username, emailVerificationUrl) => {
   return {
     body: {
@@ -40,4 +80,8 @@ const forgotPasswordMailgenContent = (username, forgotPasswordUrl) => {
   };
 };
 
-export { emailVerificationMailgenContent, forgotPasswordMailgenContent };
+export {
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+  sendMail,
+};
