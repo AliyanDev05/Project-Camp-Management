2893fca1692d09cd0a88c2481dceb6283d27f99d
AliyanDev05 <aliyanabbas2812@gmail.com>
2026-08-08 13:35:04 +0500

    [PCM-0014] implemented the express-validation in register route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controllers.js";
+import { userRegisterValidation } from "../validators/index.js";
+import { validate } from "../middlewares/validate.middlewares.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router
+  .route("/register")
+  .post(userRegisterValidation(), validate, registerUser);
 
 export default router;
---
+++ src/validators/index.js
@@ -0,0 +1,26 @@
+import { body } from "express-validator";
+
+const userRegisterValidation = () => {
+  return [
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("username is required")
+      .isLength({ min: 3, max: 30 })
+      .withMessage("username must be 3 characters"),
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("email is incorrect"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long"),
+  ];
+};
+
+export { userRegisterValidation };
